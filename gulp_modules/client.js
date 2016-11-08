ddf8c854fc2851c4b6d8794ad8c53c71a78810fe
reidwmulkey <reidwmulkey@gmail.com>
2016-11-08 17:00:19 -0600

    initial commit post wooted

--- gulp_modules/client.js
+++ gulp_modules/client.js
@@ -21,139 +21,108 @@ else
 module.exports.build = {}, module.exports.watch = {};
 
 module.exports.build.views = function() {
-	gulp.src('./assets/client/views/**/*.jade')
+	gulp.src('./assets/views/**/*.jade')
 	.pipe(jade({
 		pretty:false,
 		locals: {
 			environment: env
 		}
 	}))
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/views'))
+	.pipe(gulp.dest('./' + buildFolder + '/assets/views'))
 
-	gulp.src('./assets/client/views/index.jade')
+	gulp.src('./assets/views/index.jade')
 	.pipe(jade({
 		pretty:false,
 		locals: {
 			environment: env
 		}
 	}))
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client'))
+	.pipe(gulp.dest('./' + buildFolder + '/assets'))
 
 };
 
 module.exports.watch.views = function() {
-	gulp.watch('./assets/client/views/**/*.jade', ['client~views']);
+	gulp.watch('./assets/views/**/*.jade', ['client~views']);
 }
 
 module.exports.build.js = function() {
 	var scripts = [
-		'./assets/client/js/app.module.js',
-		'./assets/client/js/**/*.module.js',
-		'./assets/client/js/**/*.factory.js',
-		'./assets/client/js/**/*.service.js',
-		'./assets/client/js/**/*.directives.js',
-		'./assets/client/js/**/*.filters.js',
-		'./assets/client/js/**/*.controller.js',
-
-		'./assets/client/js/wooted.js',
+		'./assets/js/clean-blog.js'
 	];
 	
 	console.log(nconf.get('NODE_ENV'));
 	var stripping = false;
-	var envConfig = './assets/client/js/factories/config.factory.local.js';
-	if(env === "production"){
-		envConfig = './assets/client/js/factories/config.factory.prod.js';
+	if(env === "production" || "env" === "staging")
 		stripping = true;
-	} 
-	else if(env === "staging") {
-		envConfig = './assets/client/js/factories/config.factory.staging.js';
-		stripping = true;
-	}
-	scripts.splice(2, 0, envConfig);
-
+	
 	var pumpArray = [
 		gulp.src(scripts),
 		concat('app.js'),
-		// uglify(),
-		// stripDebug(),
-		gulp.dest('./' + buildFolder + '/assets/client/js')
+		uglify(),
+		stripDebug(),
+		gulp.dest('./' + buildFolder + '/assets/js')
 	];
-	// if(env !== "production"){
-	// 	pumpArray.splice(2,2);
-	// }
+	if(env !== "production"){
+		pumpArray.splice(2,2);
+	}
 	pump(pumpArray);
 }
 
 module.exports.watch.js = function() {
-	gulp.watch('./assets/client/js/**/*.js', ['client~js']);
+	gulp.watch('./assets/js/**/*.js', ['client~js']);
 }
 
 module.exports.build.lib = function(){
+	//takes the un-minified copies of jquery & bootstrap and compresses them into "lib.js" and sends it to the build folder
 	var libScripts = [
-		'./lib/angular/angular.js',
-		'./lib/angular-ui-router/release/angular-ui-router.min.js',
-		'./lib/angular-animate/angular-animate.js',
-		'./lib/angular-aria/angular-aria.js',
-		'./lib/angular-material/angular-material.js',
-		'./lib/angular-seo/angular-seo.js',
-		'./lib/angular-truncate/src/truncate.js',
-		'./lib/angular-material-data-table/dist/md-data-table.js',
-		'./lib/angular-material-icons/angular-material-icons.js'
+	   './lib/jquery/dist/jquery.js',
+ 	   './lib/bootstrap/dist/js/bootstrap.js'
 	];
 
 	pump([
 		gulp.src(libScripts),
 		concat('lib.js'),
 		uglify(),
-		gulp.dest('./' + buildFolder + '/assets/client/js')
+		gulp.dest('./' + buildFolder + '/assets/js')
 	]);
 }
 
 module.exports.build.css = function(){
 	var stylesheets = [
-	'./lib/angular/angular-csp.css',
-	'./lib/angular-material/angular-material.css',
-	'./lib/angular-material-data-table/dist/md-data-table-style.css',
-	'./lib/angular-material-icons/angular-material-icons.css'
+ 	   './lib/bootstrap/dist/css/bootstrap.css',
+ 	   './lib/font-awesome/css/font-awesome.css'
 	];
 
 	pump([
 		gulp.src(stylesheets),
 		concat('lib.css'),
 		uglifycss(),
-		gulp.dest('./' + buildFolder + '/assets/client/css')
+		gulp.dest('./' + buildFolder + '/assets/css')
 	]);
 }
 
 module.exports.build.extras = function() {
 	var stylesheets = [
-		'./assets/client/scss/*.scss'
+		'./assets/scss/*.scss'
 	]
 	pump([
 		gulp.src(stylesheets),
 		sass(),
 		concat('app.css'),
 		uglifycss(),
-		gulp.dest('./' + buildFolder + '/assets/client/css')
+		gulp.dest('./' + buildFolder + '/assets/css')
 	]);
 	
-	gulp.src('./assets/client/img/**/*')
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/img'));
+	gulp.src('./assets/img/**/*')
+	.pipe(gulp.dest('./' + buildFolder + '/assets/img'));
 
-	gulp.src('./assets/client/fonts/**/*')
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/fonts'));
+	gulp.src('./lib/font-awesome/fonts/**/*')
+	.pipe(gulp.dest('./' + buildFolder + '/assets/fonts'));
 
-	gulp.src('./lib/angular/angular.min.js.map')
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/js'));
-
-	gulp.src('./lib/angular-messages/angular-messages.min.js.map')
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/js'));
-
-	gulp.src('./lib/angular-local-storage/dist/angular-local-storage.min.js.map')
-	.pipe(gulp.dest('./' + buildFolder + '/assets/client/js'));
-
+	gulp.src('./assets/fonts/**/*')
+	.pipe(gulp.dest('./' + buildFolder + '/assets/fonts'));
 }
 
 module.exports.watch.extras = function(){
-	gulp.watch('./assets/client/scss/*.scss', ['client~extras']);
+	gulp.watch('./assets/scss/*.scss', ['client~extras']);
 }
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-var cron = require('node-schedule');
-var mongoose = require('mongoose');
 var express = require('express');
 var _ = require('underscore');
 var nconf = require('nconf');
 var http = require('http');
 var q = require('q');
 
-var config = require('./modules/config');
-var woot = require('./modules/woot');
-
 var router = express.Router();
-var ready = q.defer();
-var oneDay = 86400000;
+var oneDay = 86400000; //used for setting cache of /assets
+
+//determines what the node_environment is so it knows where to find the built site assets
 nconf.argv().env();
 var env = nconf.get('NODE_ENV');
 console.log('environment: ' + env);
@@ -22,88 +18,42 @@ else if(env === "production")
 	buildFolder += '/prod';
 else
 	buildFolder += '/local';
-
 console.log('build folder: ' + buildFolder);
-mongoose.Promise = q.Promise;
 
-mongoose.connect(config.mongoURL, function(err) {
-	if (err) {
-		console.error('Failed to connect to mongo on startup -', err);
-		ready.reject(err);
-	}
-	else {
-		console.log('mongo connection created.');
-		ready.resolve();
-	}
+//create the express app
+var app = express();
+
+app.use('/api', function(req, res, next) {
+	res.status(501).send();
+	//there's no API for this simple site, so return unimplemented if someone attempts to reach the API
 });
 
-q.all([ready])
-.then(function(){
-	//setup a cron to catch the daily deals
-	cron.scheduleJob('0 8 * * *', function(){
-		console.log('starting to retrieve the daily items.');
-		woot.getTodaysWoots()
-		.then(woot.storeWoots)
-		.then(function(){
-			console.log(new Date(), " - all woot items recorded successfully.");
-		})
-		.catch(console.error);
-	});
+//set up the routing to build files
+app.use('/assets', express.static(__dirname + buildFolder + '/assets', {maxAge: oneDay}));
 
-	//setup a cron to catch the weekly deals (called events)
-	cron.scheduleJob('0 8 * * 1', function(){
-		console.log('starting to retrieve the weekly items.');
-		woot.getWeeksEvents()
-		.then(function(data){
-			woot.storeWoots(_.flatten(data));
-		})
-		.then(function(){
-			console.log(new Date(), " - all event items recorded successfully.");
-		})
-		.catch(console.error);
-	});
+router.use('/assets/*', function(req, res){
+	res.status(404).send();
+});
 
-	//create the express app
-	var app = express();
+//if they just arrive at bear-fight-betting.com, give them index.html
+router.use('/', function(req, res){
+	res.sendFile(__dirname + buildFolder + '/assets/views/app/index.html');
+});
 
-	app.use('/api', function(req, res, next) {
-		if (1 !== mongoose.connection.readyState) {
-			res.status(503).send();
-		}
-		else
-			next();
-	});
+router.use('/newsletter', function(req, res){
+	console.log('hit newsletter??');
+	res.sendFile(__dirname + buildFolder + '/assets/views/app/newsletter.html');
+});
 
-	//set up the routing to api/client
-	var api = require('./api');
+router.use('/*', function(req, res){
+	res.status(404).send();
+});
 
-	app.use('/api', api);
-	app.use('/assets', express.static(__dirname + buildFolder + '/assets', {maxAge: oneDay}));
+app.use('/', router);
 
-	router.use('/assets/*', function(req, res){
-		res.status(404).send();
-	});
-
-	router.use('/', function(req, res){
-		res.sendFile(__dirname + buildFolder + '/assets/client/index.html');
-	});
-
-	router.use('/*', function(req, res){
-		res.status(404).send();
-	});
-
-	app.use('/', router);
-
-	//error handling
-	app.use(function (err, req, res, next) {
-		res.status(500).send(err);
-	});
-
-	//create server
-	var port = config.port;
-	app.set('port', port);
-	var server = http.createServer(app);
-	server.listen(port);
-	console.log('server listening on port ' + port + '.');
-})
-.catch(console.error);
+//create the express server
+var port = 3000;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+console.log('server listening on port ' + port + '.');
